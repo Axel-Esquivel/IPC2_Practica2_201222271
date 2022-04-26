53fcef299ad8612a46f4aff70551967d937eec78
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-04-26 06:28:06 -0600

    portafolio finalizado

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
 lenguajes = [
-    { nombre: 'C Sharp', imagen: 'c-sharp.svg', nivel: 'Avanzado' },
-    { nombre: 'Java', imagen: 'java.svg', nivel: 'Avanzado' },
-    { nombre: 'HTML 5', imagen: 'html-5.svg', nivel: 'Avanzado' },
-    { nombre: 'CSS 3', imagen: 'css-3.svg', nivel: 'Intermedio' },
-    { nombre: 'Bootstrap', imagen: 'bootstrap.svg', nivel: 'Intermedio' },
-    { nombre: 'JavaScript', imagen: 'javascript.svg', nivel: 'Avanzado' },
-    { nombre: 'NodeJS', imagen: 'nodejs.svg', nivel: 'Avanzado' },
-    { nombre: 'TypeScript', imagen: 'typescript.svg', nivel: 'Avanzado' },
-    { nombre: 'Angular', imagen: 'angular.svg', nivel: 'Avanzado' },
-    { nombre: 'Ionic', imagen: 'ionic.svg', nivel: 'Avanzado' },
-    { nombre: 'NestJS', imagen: 'nestjs.svg', nivel: 'Intermedio' },
-    { nombre: 'Python', imagen: 'python.svg', nivel: 'Avanzado' },
-    { nombre: 'DJango', imagen: 'django.svg', nivel: 'Básico' },
-    { nombre: 'Flask', imagen: 'flask.svg', nivel: 'Intermedio' },
-    { nombre: 'Docker', imagen: 'docker.svg', nivel: 'Intermedio' },
-    { nombre: 'Git', imagen: 'git.svg', nivel: 'Intermedio' },
-    { nombre: 'GitHub', imagen: 'github.svg', nivel: 'Intermedio' },
-    { nombre: 'SQL Server', imagen: 'sql-server.svg', nivel: 'Intermedio' },
-    { nombre: 'MySQL', imagen: 'mysql.svg', nivel: 'Intermedio' },
-    { nombre: 'MongoDB', imagen: 'mongodb.svg', nivel: 'Intermedio' },
-    { nombre: 'Windows Server', imagen: 'windows-10.svg', nivel: 'Intermedio' },
-    { nombre: 'Excel', imagen: 'excel.svg', nivel: 'Avanzado' },
-    { nombre: 'Word', imagen: 'word.svg', nivel: 'Avanzado' },
-    { nombre: 'PowerPoint', imagen: 'power-point.svg', nivel: 'Avanzado' },
+    { nombre: 'C Sharp', imagen: 'c-sharp.svg', nivel: 90 },
+    { nombre: 'Java', imagen: 'java.svg', nivel: 90 },
+    { nombre: 'HTML 5', imagen: 'html-5.svg', nivel: 80 },
+    { nombre: 'CSS 3', imagen: 'css-3.svg', nivel: 60 },
+    { nombre: 'Bootstrap', imagen: 'bootstrap.svg', nivel: 80 },
+    { nombre: 'JavaScript', imagen: 'javascript.svg', nivel: 90 },
+    { nombre: 'NodeJS', imagen: 'nodejs.svg', nivel: 80 },
+    { nombre: 'TypeScript', imagen: 'typescript.svg', nivel: 90 },
+    { nombre: 'Angular', imagen: 'angular.svg', nivel: 90 },
+    { nombre: 'Ionic', imagen: 'ionic.svg', nivel: 90 },
+    { nombre: 'NestJS', imagen: 'nestjs.svg', nivel: 80 },
+    { nombre: 'Python', imagen: 'python.svg', nivel: 90 },
+    { nombre: 'DJango', imagen: 'django.svg', nivel: 50 },
+    { nombre: 'Flask', imagen: 'flask.svg', nivel: 70 },
+    { nombre: 'Docker', imagen: 'docker.svg', nivel: 60 },
+    { nombre: 'Git', imagen: 'git.svg', nivel: 80 },
+    { nombre: 'GitHub', imagen: 'github.svg', nivel: 70 },
+    { nombre: 'SQL Server', imagen: 'sql-server.svg', nivel: 70 },
+    { nombre: 'MySQL', imagen: 'mysql.svg', nivel: 70 },
+    { nombre: 'MongoDB', imagen: 'mongodb.svg', nivel: 60 },
+    { nombre: 'Windows Server', imagen: 'windows-10.svg', nivel: 60 },
+    { nombre: 'Excel', imagen: 'excel.svg', nivel: 95 },
+    { nombre: 'Word', imagen: 'word.svg', nivel: 80 },
+    { nombre: 'PowerPoint', imagen: 'power-point.svg', nivel: 80 },
 ]
 
 function inicio() {
@@ -38,13 +38,13 @@ function inicio() {
         imagen = document.createElement('img');
         imagen.src = `media/logos/${ lenguaje[1].imagen }`;
 
-        nivel = document.createElement('div');
-        nivel.classList.add('estrellas');
-        nivel.innerHTML = `<strong>Nivel: </strong> ${ lenguaje[1].nivel }`;
+        progreso = document.createElement('div');
+        progreso.classList.add('progress');
+        progreso.innerHTML =  `<div class="progress-bar" role="progressbar" style="width: ${ lenguaje[1].nivel }%" aria-valuenow="${ lenguaje[1].nivel }" aria-valuemin="0" aria-valuemax="100">${ lenguaje[1].nivel }%</div>`;
 
         logo.appendChild(nombre);
         logo.appendChild(imagen);
-        logo.appendChild(nivel)
+        logo.appendChild(progreso)
 
         logos.appendChild(logo);
     });
